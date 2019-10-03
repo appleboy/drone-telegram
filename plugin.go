7292051d07116f00bf4c638d1205d1ccad6bcbb6
Dennis Speranskiy <denis@speran.info>
2019-10-03 08:17:34 +0300

    if proxy set then use proxy

--- plugin.go
+++ plugin.go
@@ -236,9 +236,18 @@ func (p Plugin) Exec() error {
 		message = p.Message()
 	}
 
-  proxyUrl, err := url.Parse(p.Config.Socks5)
-  proxyClient := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)}}
-  bot, err := tgbotapi.NewBotAPIWithClient(p.Config.Token, proxyClient)
+  var err error
+  var bot *tgbotapi.BotAPI
+  var proxyUrl *url.URL
+  if len(p.Config.Socks5) > 0 {
+    proxyUrl, err := url.Parse(p.Config.Socks5)
+    proxyClient := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)}}
+    bot, err = tgbotapi.NewBotAPIWithClient(p.Config.Token, proxyClient)
+    return err
+  } else {
+    bot, err = tgbotapi.NewBotAPI(p.Config.Token)
+    return err
+  }
 
 	if err != nil {
 		return err
